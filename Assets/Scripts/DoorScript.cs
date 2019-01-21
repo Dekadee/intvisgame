3050d9ad7eb85570f180e260abd761b67e9f46b7
Dekadee <dominic.ebner@outlook.com>
2019-01-21 14:21:54 +0100

    Adding opening scene

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,101 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Demonstrates how to create a simple interactable object
+//
+//=============================================================================
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    //-------------------------------------------------------------------------
+    [RequireComponent(typeof(Interactable))]
+    public class DoorScript : MonoBehaviour
+    {
+
+        private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+        private Interactable interactable;
+
+        //-------------------------------------------------
+        void Awake()
+        {
+            interactable = this.GetComponent<Interactable>();
+        }
+
+
+        //-------------------------------------------------
+        // Called when a Hand starts hovering over this object
+        //-------------------------------------------------
+        private void OnHandHoverBegin(Hand hand)
+        {
+
+        }
+
+
+        //-------------------------------------------------
+        // Called when a Hand stops hovering over this object
+        //-------------------------------------------------
+        private void OnHandHoverEnd(Hand hand)
+        {
+
+        }
+
+
+        //-------------------------------------------------
+        // Called every Update() while a Hand is hovering over this object
+        //-------------------------------------------------
+        private void HandHoverUpdate(Hand hand)
+        {
+           if(SteamVR_Input.__actions_default_in_GrabPinch.GetStateDown(hand.handType))
+           {
+                SceneManager.LoadScene("TestScene");
+           }
+        }
+
+
+        //-------------------------------------------------
+        // Called when this GameObject becomes attached to the hand
+        //-------------------------------------------------
+        private void OnAttachedToHand(Hand hand)
+        {
+
+        }
+
+
+        //-------------------------------------------------
+        // Called when this GameObject is detached from the hand
+        //-------------------------------------------------
+        private void OnDetachedFromHand(Hand hand)
+        {
+
+        }
+
+
+        //-------------------------------------------------
+        // Called every Update() while this GameObject is attached to the hand
+        //-------------------------------------------------
+        private void HandAttachedUpdate(Hand hand)
+        {
+
+        }
+
+
+        //-------------------------------------------------
+        // Called when this attached GameObject becomes the primary attached object
+        //-------------------------------------------------
+        private void OnHandFocusAcquired(Hand hand)
+        {
+        }
+
+
+        //-------------------------------------------------
+        // Called when another attached GameObject becomes the primary attached object
+        //-------------------------------------------------
+        private void OnHandFocusLost(Hand hand)
+        {
+        }
+    }
+}
