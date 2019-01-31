805f00b78408f0d3e04fc56abf87cdfb2e8d8677
Dekadee <dominic.ebner@outlook.com>
2019-01-31 17:02:46 +0100

    Some stuff

---
+++ Assets/Scripts/GeneratorHatch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GeneratorHatch : MonoBehaviour {
+
+    [SerializeField]
+    private Collider handleCol;
+    [SerializeField]
+    private LinearMapping mapping;
+    [SerializeField]
+    private AudioSource audioHatch;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(mapping.value > 0.5)
+        {
+            handleCol.enabled = true;
+        }
+        else
+        {
+            handleCol.enabled = false;
+        }
+	}
+}
