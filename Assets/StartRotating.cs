07585921ff3c98756d8467b5e03ae640f563a376
Dekadee <dominic.ebner@outlook.com>
2019-02-06 15:38:17 +0100

    Catch up with presentation version

---
+++ Assets/StartRotating.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRotating : MonoBehaviour {
+
+    private bool rotate = false;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotate)
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+            canvas.SetActive(true);
+        }
+       
+	}
+
+    public void startRotation()
+    {
+        rotate = true;
+    }
+}
